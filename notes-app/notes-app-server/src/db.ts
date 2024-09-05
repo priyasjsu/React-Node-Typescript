bfc9395be67413abec3d205e6a4c5069bc06e3e5
priyasjsu <priya.khandelwal@sjsu.edu>
2024-09-05 15:12:51 -0700

    Update env

--- notes-app/notes-app-server/src/db.ts
+++ notes-app/notes-app-server/src/db.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const pool: Pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'qwsa@123',
+  password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'note-app',
   waitForConnections: true,
   connectionLimit: 10,
